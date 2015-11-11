a501bb6a15b212577083c67c5ba0e97094197085
LucasSdS <lucassantos10@gmail.com>
2015-11-11 01:17:37 -0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-				
-	}
+		public static void main(String[] args) throws VetorNullPointerException, VetorIndexOutOfBoundsException {
 
+ 			Vetor v = new Vetor(3);
+ 			v.put(0,1);
+ 			v.put(2,5);
+ 			System.out.println("Vetor 1: ");
+	 		v.print();
+	 		v.sort();
+ 		}
+	}
 }
